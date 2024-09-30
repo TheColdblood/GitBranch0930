1390286bbea10d0b7bac2669cf764e8daedde3e3
TheColdblood <simongregory05@gmail.com>
2024-09-30 13:23:24 +0200

    non static

--- src/gitbranch/GitBranch.java
+++ src/gitbranch/GitBranch.java
@@ -5,10 +5,11 @@ public class GitBranch {
 
     
     public static void main(String[] args) {
-        int i = fv();
+        GitBranch obj = new GitBranch();
+        int i = obj.fv();
     }
 
-    private static int fv() {
+    private int fv() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
